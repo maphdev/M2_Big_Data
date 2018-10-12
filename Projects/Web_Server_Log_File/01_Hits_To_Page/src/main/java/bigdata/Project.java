acad02d4e4f68f31e932ea1c44e836e573167883
Manon Philippot <manon.philippot@gmail.com>
2018-10-12 20:09:46 +0200

    Web Servers Log File part 01

---
+++ Projects/Web_Server_Log_File/01_Hits_To_Page/src/main/java/bigdata/Project.java
@@ -0,0 +1,69 @@
+package bigdata;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+// 10.223.157.186 - - [15/Jul/2009:15:50:35 -0700] "GET /assets/js/lowpro.js HTTP/1.1" 200 10469
+// 11111111111111 2 3 444444444444444444444 555555 6666 77777777777777777777 888888888 999 10000
+
+public class Project {
+  public static class ProjectMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+	  public void map(Object key, Text value, Context context
+			  ) throws IOException, InterruptedException {
+		  String line = value.toString();
+		  String[] tokens = line.split(" ");
+		  context.write(new Text(tokens[6]), new IntWritable(1));
+	  }
+  }
+  public static class ProjectReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+    	String oldPath = "";
+    	int nbHits = 0;
+    	for(IntWritable value: values){
+    		String thisPath = key.toString();
+    		if(!oldPath.equals("") && !oldPath.equals(thisPath)){
+    			context.write(new Text(oldPath), new IntWritable(nbHits));
+    			nbHits = 0;
+    		}
+    		oldPath = thisPath;
+    		nbHits+=1;
+    	}
+    	if(!oldPath.equals("")){
+			context.write(new Text(oldPath), new IntWritable(nbHits));
+    	}
+    }
+  }
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "Project");
+    job.setNumReduceTasks(1);
+    job.setJarByClass(Project.class);
+    job.setMapperClass(ProjectMapper.class);
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setReducerClass(ProjectReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+    job.setInputFormatClass(TextInputFormat.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
